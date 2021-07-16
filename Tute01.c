4f79f70facb89a430cfe8b64241ce55ab4cbcede
IT21114144 <87514599+IT21114144@users.noreply.github.com>
2021-07-16 06:20:52 +0000

    Completed - Tutorial 01

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,26 @@
 #include <stdio.h>
 
 int main() {
-  
+
+  float dis , amt ;
+
+  printf("Enter the Distance : ") ;
+  scanf( "%f" , &dis ) ;
+
+  if ( dis < 30 )
+  {
+    amt = 30 * 50  ;
+  } 
+  else if ( dis > 30 )
+  {
+    amt = 30 * 50 + ( dis - 30 ) * 40 ;
+  } 
+  else 
+  {
+    printf(" Invalid Disstance ") ;
+  }
+
+  printf("\nThe Amount to be paid %.2f" , amt) ;
   return 0;
 }
 
